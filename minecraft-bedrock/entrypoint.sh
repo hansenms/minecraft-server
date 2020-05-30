faf79c739536da878fc83f9a12d71e85e4953b20
Michael Hansen <cloudynerd@outlook.com>
2020-05-30 22:53:21 +0000

    Started adjusting helm chart for new docker image

--- minecraft-bedrock/entrypoint.sh
+++ minecraft-bedrock/entrypoint.sh
@@ -4,7 +4,31 @@ rm -f bedrock_server *.so 2> /dev/null
 unzip -n -q /tmp/bds.zip # Don't overwrite
 chmod +x bedrock_server
 
+# Set configuration values provided in environment variables
+CONFIG="server.properties"
+function set_config(){
+    sed -i "s/^\($1\s*=\s*\).*\$/\1$2/" $CONFIG
+}
+
+if [ ! -z "${SERVER_NAME}" ]; then
+    set_config "server-name" $SERVER_NAME
+fi
+
+if [ ! -z "${MAX_PLAYERS}" ]; then
+    set_config "max-players" $MAX_PLAYERS
+fi
+
+if [ ! -z "${ONLINE_MODE}" ]; then
+set_config "online-mode" $ONLINE_MODE
+fi
+
+if [ ! -z "${DIFFICULTY}" ]; then
+set_config "difficulty" $DIFFICULTY
+fi
+
+if [ ! -z "${GAMEMODE}" ]; then
+set_config "gamemode" $GAMEMODE
+fi
+
 export LD_LIBRARY_PATH=.
 screen -S bedrock /opt/bedrock/run_bedrock.sh
-
-# ./bedrock_server

589b48b62858afa1d1103aaa4e0ba4c2c26068a8
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2021-03-19 22:29:44 -0400

    Backup scripts improvement
    - Add k8s namespace as an optional 3rd argument to `bedrock-backup.sh`
    - Variables quoting as per <https://github.com/koalaman/shellcheck/wiki/SC2086>
    - Few minor improvements (arguments validation, etc)

--- bedrock-backup/pod-get-bedrock-process-id.sh
+++ bedrock-backup/pod-get-bedrock-process-id.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 
 for pid in $(cd /proc && echo +([0-9])); do 
-    if [ -f /proc/$pid/cmdline ]; then 
-        grep -q "bedrock_server" /proc/$pid/cmdline; [ $? -eq 0 ] && echo -n $pid
+    if [ -f "/proc/${pid}/cmdline" ]; then
+        if grep -q "bedrock_server" "/proc/${pid}/cmdline"; then echo -n "${pid}"; fi
     fi 
 done
